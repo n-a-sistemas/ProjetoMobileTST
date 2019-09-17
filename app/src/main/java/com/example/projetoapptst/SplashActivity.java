e526117fea489412ba82d0b9019f460ae3eeb00d
drvictorh <victorhugomdossantos@gmail.com>
2019-09-17 13:58:48 -0300

    Splash concluido so falta imagem

---
+++ app/src/main/java/com/example/projetoapptst/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.projetoapptst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                main();
+            }
+        }, 2000);
+
+    }
+
+    public void main(){
+        Intent intent = new Intent(
+                SplashActivity.this,MainActivity.class
+        );
+        startActivity(intent);
+        finish();
+    }
+
+    }
+
