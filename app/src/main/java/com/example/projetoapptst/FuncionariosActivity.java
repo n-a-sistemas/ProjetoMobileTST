ec8821ce130f2e9794344dcf116cd7f869d90a31
ytaconelli <ytaconelli31@gmail.com>
2019-10-14 16:45:24 -0300

    99,965% concluído (corrigido 2.0)

--- app/src/main/java/com/example/projetoapptst/FuncionariosActivity.java
+++ app/src/main/java/com/example/projetoapptst/FuncionariosActivity.java
@@ -134,7 +134,7 @@ public class FuncionariosActivity extends AppCompatActivity {
 
     public void confirmar(final Integer ponto){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(R.string.bonificacao1);
+        builder.setTitle(R.string.bonificacaog);
         builder.setMessage("Você está prestes a atribuir " + this.ponto + " pontos, deseja prosseguir?");
         builder.setIcon(R.drawable.rules);
         builder.setPositiveButton("sim", new DialogInterface.OnClickListener() {
@@ -165,7 +165,7 @@ public class FuncionariosActivity extends AppCompatActivity {
 
     public void retirar(final Integer ponto){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(R.string.infracao1);
+        builder.setTitle(R.string.infracaog);
         builder.setMessage("Você está prestes a retirar " + this.ponto + " pontos, deseja prosseguir?");
         builder.setIcon(R.drawable.rules);
         builder.setPositiveButton("sim", new DialogInterface.OnClickListener() {
